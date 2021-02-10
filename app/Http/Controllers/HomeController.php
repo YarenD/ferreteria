ef8def2fd93af0591948d122b7ceceaf1c57c0b5
Aldo Aranza <aldo@adndigital.mx>
2021-02-10 07:58:14 -0600

    Comentario en versión inicial

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,6 +10,7 @@ class HomeController extends Controller
      * Create a new controller instance.
      *
      * @return void
+     * Se considera Auth middleware auth
      */
     public function __construct()
     {
