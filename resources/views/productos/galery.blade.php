78be9ba987d0b57be51d9301d9fc0ad66f2c6464
gab9999 <gabrielgomez_100@hotmail.com>
2021-02-11 22:51:06 -0300

    ultima modificacion en galeria

--- resources/views/productos/galery.blade.php
+++ resources/views/productos/galery.blade.php
@@ -2,15 +2,12 @@
 @section ('contenido')
 <div class="row justify-content-center align-items-center">
 	<div class="col-lg-11 col-md-11 col-sm-11 col-xs-11">
-		<h4>Galeria de Fotos</h4>
+		<h3>GALERIA DE FOTOS</h3>
 	</div>
 </div>
 <div class="row justify-content-center align-items-center">
 </div>
 <br>
-{{-- @php
-	$counts = 0;
-@endphp --}}
 <div class="row justify-content-center align-items-center">
 	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
 		@foreach ($prod as $item)
